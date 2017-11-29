ac614a17c64503190c7a21b5163dd0a9fb25d702
tfung92 <tfung92@users.noreply.github.com>
2017-11-28 21:27:32 -0500

    add examples from class
    added more examples

--- Week_05/js-starting-out/js/main.js
+++ Week_05/js-starting-out/js/main.js
@@ -11,3 +11,29 @@ function leftTurnOnly() {
   //instructions
   console.log(directions);
 }
+
+//calling a function
+leftTurnOnly();
+
+
+//Undefined variable
+var someVariable;
+
+console.log("hello world")
+
+var string1 = "tiffany";
+var string2 = "fung";
+var string3 = "!";
+var num1 = 100;
+var num2 = 24;
+var num3 = 52;
+var nothing = null;
+var boolean1 = true;
+
+function multiply(num1){
+ num1*10
+}
+
+console.log(num1*10)
+console.log(90/9)
+console.log(string1+" "+string2);
