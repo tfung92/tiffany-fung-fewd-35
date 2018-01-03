74df84392fec03fecf2f64b804d1b73e58be6d40
tfung92 <tfung92@users.noreply.github.com>
2018-01-02 19:17:40 -0500

    committing latest progress

--- final_project/js/setup_page.js
+++ final_project/js/setup_page.js
@@ -1,5 +1,6 @@
 $('#steps').progressbar({
-  // ~: failed
-  // @: current
-  steps: ['Setp 1', '~Step 2 Failed', '@Step 3 ( Current )', 'Step 4',]
+  //notation as follows:
+  // ~: step failed
+  // @: current step
+  steps: ['Step 1', 'Step 2', '@Step 3 ( Current )', 'Step 4',]
 });
